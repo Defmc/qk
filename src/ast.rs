f55e5389350cf0d984fed10b346ff1522f4840d1
Defmc <defmc0101@gmail.com>
2026-02-28 19:09:40 -0300

    feat: add a prettier-printer

--- src/ast.rs
+++ src/ast.rs
@@ -11,6 +11,31 @@ pub enum Ast {
     Var,
 }
 
+pub fn display_node(n: &Node) {
+    fn span_str(span: &SourceSpan) -> String {
+        format!("{}..{}", span.offset(), span.offset() + span.len())
+    }
+
+    fn indented(n: &Node, mut depth: usize) {
+        print!("{}", " ".repeat(depth * 2));
+        depth += 1;
+        let span = span_str(&n.at);
+        match &n.item {
+            Ast::Var => println!("ν @ {span}"),
+            Ast::Abs(v, inner) => {
+                println!("λ {} @ {span} ∈", span_str(&v));
+                indented(&inner, depth);
+            }
+            Ast::App(l, r) => {
+                println!("⋅ @ {span}");
+                indented(&l, depth);
+                indented(&r, depth);
+            }
+        }
+    }
+    indented(n, 0)
+}
+
 impl Ast {
     pub fn at(self, at: SourceSpan) -> Node {
         Meta { item: self, at }.into()
