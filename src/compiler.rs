48322b8c9500706a38da163c4a021b23307f3033
Defmc <defmc0101@gmail.com>
2026-03-02 15:33:18 -0300

    fix(compiler): update to use IR structure

--- src/compiler.rs
+++ src/compiler.rs
@@ -1,9 +1,12 @@
-use std::fmt;
+use std::{collections::HashMap, fmt};
 
 use miette::{Diagnostic, SourceSpan};
 use thiserror::Error;
 
-use crate::ast::{Ast, Node};
+use crate::{
+    ast::{Ast, Node},
+    ir::{self, IrComponent, IrObj, Scope},
+};
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct TermIdx(usize);
@@ -29,73 +32,127 @@ pub enum Error {
         #[label("this ident is unknown here")]
         at: SourceSpan,
     },
+
+    #[error(transparent)]
+    #[diagnostic(transparent)]
+    IrCompiler(#[from] crate::ir::Error),
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-#[derive(Debug, Default)]
-pub struct Compiler {
+#[derive(Debug)]
+pub struct CodeUnit<'a> {
     pub pool: Vec<Term>,
+    pub scope: &'a mut Scope,
+    pub src: &'a str,
+    pub obj_cache: HashMap<ir::Id, TermIdx>,
+    pub layer_stack: Vec<ir::Id>,
 }
 
-impl fmt::Display for Compiler {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("[ ")?;
-        for (i, t) in self.pool.iter().enumerate() {
-            if i > 0 {
-                f.write_str(", ")?;
-            }
-            match t {
-                Term::Var(OuterIdx(idx)) => write!(f, "ν{idx}")?,
-                Term::Abs {
-                    inner: TermIdx(idx),
-                } => write!(f, "λ{idx}")?,
-                Term::App(TermIdx(l), TermIdx(r)) => write!(f, "{l}⋅{r}")?,
-            }
+pub fn print_pool(pool: &[Term]) {
+    print!("[ ");
+    for (i, t) in pool.iter().enumerate() {
+        if i > 0 {
+            print!(", ");
         }
-        f.write_str(" ]")?;
-        Ok(())
+        match t {
+            Term::Var(OuterIdx(idx)) => print!("ν{idx}"),
+            Term::Abs {
+                inner: TermIdx(idx),
+            } => print!("λ{idx}"),
+            Term::App(TermIdx(l), TermIdx(r)) => print!("{l}⋅{r}"),
+        }
     }
+    print!(" ]");
 }
 
-impl Compiler {
-    pub fn compile(ast: &Node, src: &str) -> Result<Self> {
-        let mut s = Self::default();
-        let mut scopes = Vec::new();
-        s.compile_node(&mut scopes, ast, src)?;
+impl<'a> CodeUnit<'a> {
+    pub fn new(scope: &'a mut Scope, src: &'a str) -> Result<Self> {
+        scope.check_for_pendings()?;
+        let s = Self {
+            pool: Vec::new(),
+            scope,
+            src,
+            obj_cache: HashMap::new(),
+            layer_stack: Vec::new(),
+        };
         Ok(s)
     }
 
-    fn compile_node<'a>(
-        &mut self,
-        scopes: &mut Vec<&'a str>,
-        ast: &Node,
-        src: &'a str,
-    ) -> Result<TermIdx> {
-        match &ast.item {
-            Ast::Var => {
-                let var_name = ast.from_code(src);
-                let (id, _) = scopes
-                    .iter()
-                    .rev()
-                    .enumerate()
-                    .find(|(_, s)| **s == var_name)
-                    .ok_or(Error::UndeclaredVariable { at: ast.at })?;
-                Ok(self.push(Term::Var(OuterIdx(id))))
+    pub fn compile(&mut self, ir: &IrObj) -> Result<TermIdx> {
+        // match &ir.item {
+        //     Ast::Var => {
+        //         let var_name = ir.from_code(src);
+        //         let (id, _) = scopes
+        //             .iter()
+        //             .rev()
+        //             .enumerate()
+        //             .find(|(_, s)| **s == var_name)
+        //             .ok_or(Error::UndeclaredVariable { at: ir.at })?;
+        //         Ok(self.push(Term::Var(OuterIdx(id))))
+        //     }
+        //     Ast::Abs(v, inner) => {
+        //         let var_name = &src[v.offset()..v.offset() + v.len()];
+        //         scopes.push(var_name);
+        //         let inner = self.compile_node(scopes, inner, src)?;
+        //         scopes.pop();
+        //         Ok(self.push(Term::Abs { inner }))
+        //     }
+        //     Ast::App(l, r) => {
+        //         let l = self.compile_node(scopes, l, src)?;
+        //         let r = self.compile_node(scopes, r, src)?;
+        //         Ok(self.push(Term::App(l, r)))
+        //     }
+        //     _ => todo!(),
+        // }
+        match &ir.item {
+            IrComponent::Pending => {
+                unreachable!("`Scope::check_for_pendings` wasn't executed or is bugged")
             }
-            Ast::Abs(v, inner) => {
-                let var_name = &src[v.offset()..v.offset() + v.len()];
-                scopes.push(var_name);
-                let inner = self.compile_node(scopes, inner, src)?;
-                scopes.pop();
+            IrComponent::Binding => {
+                unreachable!("this `TermIdx` shouldn't be the entry point for compilation")
+            }
+            IrComponent::Abs(id, body) => {
+                self.layer_stack.push(*id);
+                let inner = self.compile(body)?;
+                self.layer_stack.pop();
                 Ok(self.push(Term::Abs { inner }))
             }
-            Ast::App(l, r) => {
-                let l = self.compile_node(scopes, l, src)?;
-                let r = self.compile_node(scopes, r, src)?;
+            IrComponent::App(l, r) => {
+                let l = self.compile(l)?;
+                let r = self.compile(r)?;
                 Ok(self.push(Term::App(l, r)))
             }
-            _ => todo!(),
+            IrComponent::Var(id) => match &self.scope.res_pool[id.0].item {
+                IrComponent::Def { .. } => self.compile_resource(*id),
+                IrComponent::Binding => {
+                    let outer_idx = self
+                        .layer_stack
+                        .iter()
+                        .rev()
+                        .enumerate()
+                        .find(|(_, sid)| *sid == id)
+                        .unwrap()
+                        .0;
+                    Ok(self.push(Term::Var(OuterIdx(outer_idx))))
+                }
+                _ => unreachable!(),
+            },
+            IrComponent::Def { .. } => unreachable!(
+                "`IrComponent::Def` should be a guard over a definition, not a IrC output"
+            ),
+        }
+    }
+
+    pub fn compile_resource(&mut self, res_id: ir::Id) -> Result<TermIdx> {
+        if let Some(idx) = self.obj_cache.get(&res_id) {
+            Ok(*idx)
+        } else {
+            // TODO remove this clone
+            let res = self.scope.res_pool[res_id.0].clone();
+            let compiled = self.compile(&res)?;
+            self.obj_cache.insert(res_id, compiled);
+            Ok(compiled)
         }
     }
 
--- src/ir.rs
+++ src/ir.rs
@@ -1,16 +1,19 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, hash_map::Entry};
 
 use miette::{Diagnostic, SourceSpan};
 use thiserror::Error;
 
-use crate::ast::{Ast, Node};
+use crate::{
+    ast::{Ast, Node},
+    lexer::{Meta, Trace},
+};
 
-pub type IrObj = Box<IrComponent>;
+pub type IrObj = Box<Meta<IrComponent>>;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-pub struct Id(usize);
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Id(pub usize);
 
-#[derive(Default, Debug)]
+#[derive(Default, Debug, PartialEq, Eq, PartialOrd, Ord, Clone)]
 pub enum IrComponent {
     Var(Id),
     App(IrObj, IrObj),
@@ -19,7 +22,7 @@ pub enum IrComponent {
     /// a definition
     /// i. e, a ident that represents another IrComponent
     /// e. g, I = \x.x, where I is the Def for I and \x.x the IrObj
-    Def,
+    Def(IrObj),
 
     #[default]
     Pending,
@@ -48,56 +51,70 @@ pub enum Error {
         #[label("this shouldn't be here")]
         at: SourceSpan,
     },
+
+    #[error("duplicated definition of {name:?}")]
+    #[diagnostic(
+        code(ir::duplicated_definition),
+        help("shadowing is only allow in function scopes")
+    )]
+    DuplicatedDefinition {
+        name: Box<str>,
+
+        #[label("{name:?} is first defined here")]
+        first: SourceSpan,
+
+        #[label("afterwards, it's again defined here")]
+        second: SourceSpan,
+    },
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
 /// the first
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct IrCompiler {
-    scope: Scope,
+    pub scope: Scope,
 }
 
 impl IrCompiler {
     pub fn compile(&mut self, ast: Node, src: &str) -> Result<IrObj> {
         match ast.item {
-            Ast::Var => Ok(IrComponent::Var(self.scope.get(ast.from_code(src))).into()),
+            Ast::Var => {
+                Ok(IrComponent::Var(self.scope.get_or_reserve(ast.from_code(src))?).at(ast.at))
+            }
             Ast::App(l, r) => {
-                Ok(IrComponent::App(self.compile(l, src)?, self.compile(r, src)?).into())
+                Ok(IrComponent::App(self.compile(l, src)?, self.compile(r, src)?).at(ast.at))
             }
-            Ast::Abs(v, inner) => self.guard(crate::lexer::from_code(v, src), |s, _id| {
-                s.compile(inner, src)
+            Ast::Abs(v, inner) => self.guard(crate::lexer::from_code(v, src), ast.at, |s, id| {
+                Ok(IrComponent::Abs(id, s.compile(inner, src)?).at(ast.at))
             }),
-            Ast::Def {
-                ident,
-                params,
-                body,
-            } => {
-                let reorganized_abs = params.into_iter().rev().fold(body, |abs, param| {
-                    let abs_at = abs.at;
-                    crate::lexer::Meta {
-                        item: Ast::Abs(param, abs),
-                        at: crate::lexer::over(param, abs_at),
-                    }
-                    .into()
-                });
-                let inner = self.compile(reorganized_abs, src)?;
-                self.scope
-                    .push(crate::lexer::from_code(ident, src).into(), inner);
-                Ok(IrComponent::Def.into())
-            }
-            Ast::Program(..) => unimplemented!(),
+            Ast::Def { .. } | Ast::Program(..) => unimplemented!(),
         }
     }
 
     pub fn compile_program(&mut self, ast: Node, src: &str) -> Result<()> {
         if let Ast::Program(steps) = ast.item {
             for step in steps {
-                match &step.item {
+                match step.item {
                     Ast::Var | Ast::App(..) | Ast::Abs(..) => {
                         return Err(Error::ForbiddenExprPlacement { at: step.at });
                     }
-                    Ast::Def { .. } => {
-                        self.compile(step, src)?;
+                    Ast::Def {
+                        ident,
+                        params,
+                        body,
+                    } => {
+                        let reorganized_abs = params.into_iter().rev().fold(body, |abs, param| {
+                            let abs_at = abs.at;
+                            crate::lexer::Meta {
+                                item: Ast::Abs(param, abs),
+                                at: crate::lexer::over(param, abs_at),
+                            }
+                            .into()
+                        });
+                        let inner = self.compile(reorganized_abs, src)?;
+                        let obj = IrComponent::Def(inner).at(ast.at);
+                        self.scope
+                            .push(crate::lexer::from_code(ident, src).into(), obj);
                     }
                     Ast::Program(..) => unreachable!(),
                 }
@@ -108,8 +125,13 @@ impl IrCompiler {
         }
     }
 
-    pub fn guard<T>(&mut self, name: &str, f: impl FnOnce(&mut Self, Id) -> T) -> T {
-        let id = self.scope.push_res(IrComponent::Binding.into());
+    pub fn guard<T>(
+        &mut self,
+        name: &str,
+        binding_span: SourceSpan,
+        f: impl FnOnce(&mut Self, Id) -> T,
+    ) -> T {
+        let id = self.scope.push_res(IrComponent::Binding.at(binding_span));
         let old_id = if let Some(old_id) = self.scope.definitions.get_mut(name) {
             let mut new_id = id;
             std::mem::swap(&mut new_id, old_id);
@@ -127,17 +149,34 @@ impl IrCompiler {
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct Scope {
-    res_pool: Vec<IrObj>,
-    definitions: HashMap<Box<str>, Id>,
+    pub res_pool: Vec<IrObj>,
+    pub definitions: HashMap<Box<str>, Id>,
 }
 
 impl Scope {
-    pub fn push(&mut self, name: Box<str>, res: IrObj) -> Id {
+    pub fn push(&mut self, name: Box<str>, res: IrObj) -> Result<Id> {
         let id = self.push_res(res);
-        self.definitions.insert(name.into(), id);
-        id
+        match self.definitions.entry(name) {
+            Entry::Vacant(e) => {
+                e.insert(id);
+            }
+            Entry::Occupied(mut e) => {
+                let old = &self.res_pool[e.get().0];
+                if !matches!(old.item, IrComponent::Pending) {
+                    let e = Error::DuplicatedDefinition {
+                        name: e.key().clone(),
+                        first: old.at,
+                        second: self.res_pool[id.0].at,
+                    };
+                    return Err(e);
+                } else {
+                    e.insert(id);
+                }
+            }
+        }
+        Ok(id)
     }
 
     pub fn push_res(&mut self, res: IrObj) -> Id {
@@ -148,9 +187,9 @@ impl Scope {
 
     /// if `Scope` doesn't have a resource, it doesn't mean the resource doesn't exist
     /// it only means it hasn't been evaluated yet
-    pub fn get(&mut self, s: &str) -> Id {
+    pub fn get_or_reserve(&mut self, s: &str) -> Result<Id> {
         if let Some(id) = self.definitions.get(s) {
-            return *id;
+            return Ok(*id);
         } else {
             self.push(s.into(), IrComponent::Pending.at((0..=0).into()))
         }
