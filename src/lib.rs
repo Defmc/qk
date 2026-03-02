4880ba0aaf616833cff435d5014bb55aa92280f4
Defmc <defmc0101@gmail.com>
2026-03-02 04:14:59 -0300

    refact: remove useless `Term` definition

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,5 @@
-use std::fmt::Display;
-
 pub mod ast;
 pub mod compiler;
+pub mod ir;
 pub mod lexer;
 pub mod parser;
-pub mod pool_lambda;
-
-pub type Term = Box<Body>;
-
-pub enum Body {
-    Var(usize),
-    App(Term, Term),
-    Abs(usize, Term),
-}
-
-impl Display for Body {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Var(v) => write!(f, "{v}"),
-            Self::App(l, r) => {
-                if matches!(**l, Self::Abs(..)) {
-                    write!(f, "({l}) ")?
-                } else {
-                    write!(f, "{l} ")?
-                }
-                if matches!(**r, Self::App(..)) {
-                    write!(f, "({r})")
-                } else {
-                    write!(f, "{r}")
-                }
-            }
-            Self::Abs(v, b) => write!(f, "λ{v}.{b}"),
-        }
-    }
-}
