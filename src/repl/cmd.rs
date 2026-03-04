6aca403176dcdfdeb0f8700bea3bface43c179c8
Defmc <defmc0101@gmail.com>
2026-03-04 00:33:09 -0300

    feat: add `help`

--- src/repl/cmd.rs
+++ src/repl/cmd.rs
@@ -8,48 +8,9 @@ pub struct Command<'a> {
     pub func: &'a dyn Fn(&mut Repl, &str) -> Result<()>,
 }
 
-fn quit_cmd(_r: &mut Repl, _input: &str) -> Result<()> {
-    std::process::exit(0);
-}
-
-fn set_cmd(r: &mut Repl, input: &str) -> Result<()> {
-    fn set<T: for<'a> TryFrom<&'a str>>(prop: &mut T, setting: &str, value: &str) -> Result<()> {
-        *prop = value
-            .try_into()
-            .map_err(|_| Error::InvalidValue(setting.to_string(), value.to_string()))?;
-        Ok(())
-    }
-
-    let (setting, value) = input
-        .split_once(' ')
-        .ok_or_else(|| Error::MissingArg("setting".to_string()))?;
-    match setting {
-        "prompt" => set(&mut r.prompt, "prompt", value)?,
-        "bench" => {
-            r.runner.bench = crate::repl::runner::BENCH_SETTING
-                .parse_inspired(value)
-                .map_err(|v| Error::InvalidValue(setting.to_string(), v.to_string()))?
-        }
-        "show" => {
-            r.runner.show = crate::repl::runner::SHOW_SETTING
-                .parse_inspired(value)
-                .map_err(|v| Error::InvalidValue(setting.to_string(), v.to_string()))?
-        }
-        _ => return Err(Error::UnknownSetting(setting.to_string())),
-    }
-    Ok(())
-}
-
-fn context_cmd(r: &mut Repl, input: &str) -> Result<()> {
-    for (k, v) in r.runner.irc.scope.definitions.iter() {
-        if input.is_empty() || **k == *input {
-            print!("{k} = ");
-            r.runner
-                .irc
-                .scope
-                .pretty_print(&r.runner.irc.scope.res_pool[v.0]);
-            println!();
-        }
+impl<'a> Command<'a> {
+    pub fn matches(&self, name: &str) -> bool {
+        self.cmd == name || self.alias == name
     }
     Ok(())
 }
