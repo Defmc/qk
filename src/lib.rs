5401633c614bb96e51796f9fd870549ac1e776c4
Defmc <defmc0101@gmail.com>
2026-03-17 17:49:29 -0300

    fix(temp): ignore `unused_assignments` caused by `miette` macros bug

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
+// miette's code generator is poorly handling the unused variables. Such thing is causing the
+// compiler to generate warnings that I can't suppress in the struct-level. A fix is coming. Until
+// then, a crate-level flag is necessary.
+// https://github.com/rust-lang/rust/issues/147648
+#![allow(unused_assignments)]
+
 pub mod arts;
 pub mod ast;
 pub mod compiler;
