bc3986eb9df149d115355e990926a8c53277b2be
Defmc <defmc0101@gmail.com>
2026-04-24 16:41:18 -0300

    style: use functions as parsers identity

--- src/padam/parser.rs
+++ src/padam/parser.rs
@@ -40,131 +40,96 @@ impl Error {
 }
 
 pub type Result<T> = std::result::Result<T, Error>;
-pub type NonTerminals = HashMap<Box<str>, Box<dyn Parser<Ast>>>;
+pub type NonTerminals = HashMap<Box<str>, Parser<Ast>>;
 
-pub trait Parser<T> {
-    fn parse<'a>(
-        &self,
-        nt: &'a NonTerminals,
-        lex: &'a Lexer,
-        tks: &'a [Token],
-    ) -> Result<(T, &'a [Token])>;
+pub type CombFn<T> =
+    Box<dyn for<'a> Fn(&'a NonTerminals, &'a Lexer, &'a [Token]) -> Result<(T, &'a [Token])>>;
+
+pub struct Parser<T> {
+    pub f: CombFn<T>,
 }
 
-pub struct Seq<T> {
-    seq: Vec<Box<dyn Parser<T>>>,
-}
-
-impl<T> Seq<T> {
-    pub fn new(seq: Vec<Box<dyn Parser<T>>>) -> Self {
-        Self { seq }
+impl<T: 'static> Parser<T> {
+    pub fn new(
+        f: impl for<'a> Fn(&'a NonTerminals, &'a Lexer, &'a [Token]) -> Result<(T, &'a [Token])>
+        + 'static,
+    ) -> Self {
+        Self { f: Box::new(f) }
     }
-}
 
-impl<T> Parser<Vec<T>> for Seq<T> {
-    fn parse<'a>(
-        &self,
-        nt: &'a NonTerminals,
-        lex: &'a Lexer,
-        tks: &'a [Token],
-    ) -> Result<(Vec<T>, &'a [Token])> {
-        let mut remaining_tokens = tks;
-        let v = self
-            .seq
-            .iter()
-            .map(|parser| {
-                parser
-                    .parse(nt, lex, remaining_tokens)
-                    .inspect(|(_, rem)| remaining_tokens = rem)
-                    .map(|(p, _)| p)
-            })
-            .collect::<Result<Vec<_>>>()?;
-        Ok((v, remaining_tokens))
-    }
-}
-
-pub struct Or<T> {
-    alternatives: Vec<Box<dyn Parser<T>>>,
-}
-
-impl<T> Or<T> {
-    pub fn new(alternatives: Vec<Box<dyn Parser<T>>>) -> Self {
-        Self { alternatives }
-    }
-}
-
-impl<T> Parser<T> for Or<T> {
-    fn parse<'a>(
+    pub fn parse<'a>(
         &self,
         nt: &'a NonTerminals,
         lex: &'a Lexer,
         tks: &'a [Token],
     ) -> Result<(T, &'a [Token])> {
-        let mut biggest_err = Error::NoAlternative;
-        for alt in &self.alternatives {
-            match alt.parse(nt, lex, tks) {
-                Ok(v) => return Ok(v),
-                Err(e) => {
-                    if e.tokens_consumed() > biggest_err.tokens_consumed() {
-                        biggest_err = e;
+        (self.f)(nt, lex, tks)
+    }
+
+    pub fn seq(seq: Vec<Self>) -> Parser<Vec<T>> {
+        Parser::new(move |nt, lex, tks| {
+            let mut remaining_tokens = tks;
+            let v = seq
+                .iter()
+                .map(|parser| {
+                    parser
+                        .parse(nt, lex, remaining_tokens)
+                        .inspect(|(_, rem)| remaining_tokens = rem)
+                        .map(|(p, _)| p)
+                })
+                .collect::<Result<Vec<_>>>()?;
+            Ok((v, remaining_tokens))
+        })
+    }
+
+    pub fn or(alternatives: Vec<Self>) -> Self {
+        Self::new(move |nt, lex, tks| {
+            let mut biggest_err = Error::NoAlternative;
+            for alt in &alternatives {
+                match alt.parse(nt, lex, tks) {
+                    Ok(v) => return Ok(v),
+                    Err(e) => {
+                        if e.tokens_consumed() > biggest_err.tokens_consumed() {
+                            biggest_err = e;
+                        }
                     }
                 }
             }
-        }
-        Err(biggest_err)
-    }
-}
-
-pub struct Rep<T> {
-    parser: Box<dyn Parser<T>>,
-    min: usize,
-    max: usize,
-}
-
-impl<T> Rep<T> {
-    pub fn new(parser: Box<dyn Parser<T>>, min: usize, max: usize) -> Self {
-        Self { parser, min, max }
+            Err(biggest_err)
+        })
     }
 
-    // T+
-    pub fn plus(parser: Box<dyn Parser<T>>) -> Self {
-        Self::new(parser, 1, usize::MAX)
+    pub fn rep(parser: Self, min: usize, max: usize) -> Parser<Vec<T>> {
+        Parser::new(move |nt, lex, tks| {
+            let mut remaining_tokens = tks;
+            let mut v = Vec::new();
+
+            while v.len() < max
+                && let Ok((ast, rem)) = parser.parse(nt, lex, remaining_tokens)
+            {
+                remaining_tokens = rem;
+                v.push(ast);
+            }
+
+            if v.len() < min {
+                return Err(Error::NoEnoughRep {
+                    tks_consumed: tks.len() - remaining_tokens.len(),
+                });
+            }
+
+            return Ok((v, remaining_tokens));
+        })
     }
 
-    // T*
-    pub fn any(parser: Box<dyn Parser<T>>) -> Self {
-        Self::new(parser, 0, usize::MAX)
+    pub fn plus(parser: Self) -> Parser<Vec<T>> {
+        Self::rep(parser, 1, usize::MAX)
     }
 
-    // T?
-    pub fn option(parser: Box<dyn Parser<T>>) -> Self {
-        Self::new(parser, 0, 1)
+    pub fn any(parser: Self) -> Parser<Vec<T>> {
+        Self::rep(parser, 0, usize::MAX)
     }
-}
 
-impl<T> Parser<Vec<T>> for Rep<T> {
-    fn parse<'a>(
-        &self,
-        nt: &'a NonTerminals,
-        lex: &'a Lexer,
-        tks: &'a [Token],
-    ) -> Result<(Vec<T>, &'a [Token])> {
-        let mut remaining_tokens = tks;
-        let mut v = Vec::new();
-
-        while v.len() < self.max
-            && let Ok((ast, rem)) = self.parser.parse(nt, lex, remaining_tokens)
-        {
-            remaining_tokens = rem;
-            v.push(ast);
-        }
-
-        if v.len() < self.min {
-            return Err(Error::NoEnoughRep {
-                tks_consumed: tks.len() - remaining_tokens.len(),
-            });
-        }
-
-        return Ok((v, remaining_tokens));
+    pub fn option(parser: Self) -> Parser<Vec<T>> {
+        Self::rep(parser, 0, 1)
     }
 }
